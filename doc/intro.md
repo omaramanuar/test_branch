2ada1e809dcfbe2614869a11e64e1076f0ec5d1b
Omar Manuar <omarmaunar@gmail.com>
2014-02-20 21:26:43 -1000

    * Repository to test pushing branch to github as remote.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to test_branch
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/test_branch/core_test.clj
@@ -0,0 +1,7 @@
+(ns test-branch.core-test
+  (:require [clojure.test :refer :all]
+            [test-branch.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
